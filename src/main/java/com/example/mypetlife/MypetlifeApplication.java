9222b0ffc635f2be18be83a3da49b2bed8b6fd28
jsl1113 <jsl1113@naver.com>
2023-08-16 10:19:21 +0900

    spring init

---
+++ src/main/java/com/example/mypetlife/MypetlifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mypetlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypetlifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mypetlife/MypetlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypetlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
