722cd04214f81e07fa5a52c78032f7f0675bc8da
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-17 11:21:12 +0900

    style : 띄어쓰기 추가

--- src/main/java/com/example/mypetlife/controller/UserController.java
+++ src/main/java/com/example/mypetlife/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     public String register(@RequestBody @Validated RegisterRequestDto dto) {
 
         User user = User.createUser(dto.getUsername(), dto.getEmail(), dto.getPassword(),
-                dto.getPhone(), dto.getBirthDate(), dto.getPetSpices());
+                                    dto.getPhone(), dto.getBirthDate(), dto.getPetSpices());
 
         userService.register(user);
 
