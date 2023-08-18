009a01ca2ede54aa1116913e097cf2cbb95d1bcd
olsohee <dlthgml0108@daum.net>
2023-08-18 11:22:33 +0900

    oauth

--- src/main/java/com/example/mypetlife/config/WebSecurityConfig.java
+++ src/main/java/com/example/mypetlife/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
