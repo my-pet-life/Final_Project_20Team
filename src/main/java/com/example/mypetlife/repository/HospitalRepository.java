d6ac63d74c8f6bbd19556f964153b8cc0d12a2ef
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-19 16:51:01 +0900

    remove : 필요없는 파일 삭제

--- src/main/java/com/example/mypetlife/repository/HospitalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mypetlife.repository;
-
-import com.example.mypetlife.entity.hospital.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Long> {
-}
