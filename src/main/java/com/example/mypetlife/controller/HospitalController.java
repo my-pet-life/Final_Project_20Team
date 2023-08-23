018435a6e3602856e370f453d9961fa1457f2349
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-23 13:06:11 +0900

    feat : 동물병원 지역 정보 필터 controller 클래스 추가 #27

---
+++ src/main/java/com/example/mypetlife/controller/HospitalController.java
@@ -0,0 +1,51 @@
+package com.example.mypetlife.controller;
+
+import com.example.mypetlife.dto.hospital.DistrictResponseDto;
+import com.example.mypetlife.dto.hospital.gyeonggiDo.GyeonggiHospitalCountryResponseDto;
+import com.example.mypetlife.dto.hospital.gyeonggiDo.GyeonggiHospitalInfoResponseDto;
+import com.example.mypetlife.dto.hospital.seoul.SeoulHospitalInfoResponseDto;
+import com.example.mypetlife.service.HospitalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hospitals")
+public class HospitalController {
+    private final HospitalService hospitalService;
+
+    // 병원 조회 메인 페이지 (지도)
+
+    // 시 도 필터
+    @GetMapping("/{city}")
+    public List<DistrictResponseDto> hospitalCityFilter(@PathVariable final String city) {
+        return hospitalService.getHospitalCity(city);
+    }
+
+    // 서울 ~구 동물병원 객체 반환
+    @GetMapping("/seoul/{city}/{district}")
+    public List<SeoulHospitalInfoResponseDto> seoulHospitalDistrictFilter(@PathVariable final String city,
+                                                                          @PathVariable final String district) {
+        return hospitalService.getSeoulHospital(city, district);
+    }
+
+    // 경기도 ~시 동물병원 객체 반환 ~구 필터
+    @GetMapping("/gyeonggi/{city}/{country}")
+    public List<GyeonggiHospitalCountryResponseDto> gyeonggiDoHospitalCountryFilter(@PathVariable final String city,
+                                                                                    @PathVariable final String country) {
+        return hospitalService.getGyeonggiDoHospitalDistrict(city, country);
+    }
+
+    // 경기도 ~시 ~구 동물병원 객체 반환
+    @GetMapping("/gyeonggi/{city}/{country}/{district}")
+    public List<GyeonggiHospitalInfoResponseDto> gyeonggiHospitalStreetFilter(@PathVariable final String city,
+                                                                              @PathVariable final String country,
+                                                                              @PathVariable final String district) {
+        return hospitalService.getGyeonggiDoHospital(city, country, district);
+    }
+}
