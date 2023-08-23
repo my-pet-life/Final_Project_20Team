304bbe00216dd687338b96a4fa99c3afeebad8f0
shinjaewon99 <tlswodnjs99@naver.com>
2023-08-23 13:03:53 +0900

    feat : 서울 동물병원 지역 정보 응답 DTO 추가

---
+++ src/main/java/com/example/mypetlife/dto/hospital/seoul/SeoulHospitalInfoResponseDto.java
@@ -0,0 +1,27 @@
+package com.example.mypetlife.dto.hospital.seoul;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SeoulHospitalInfoResponseDto {
+    private String city;
+    private String district;
+    private String street;
+    private String hospitalStreetNumber;
+    private String hospitalName;
+    private Integer reviewCount;
+
+
+    public SeoulHospitalInfoResponseDto(final String city, final String district,
+                                        final String street, final String hospitalStreetNumber,
+                                        final Integer reviewCount, final String hospitalName) {
+        this.city = city;
+        this.district = district;
+        this.street = street;
+        this.hospitalStreetNumber = hospitalStreetNumber;
+        this.hospitalName = hospitalName;
+        this.reviewCount = reviewCount;
+    }
+}
