dce92880776c483baa9b0b0012734b38d6f82966
jsl1113 <jsl1113@naver.com>
2023-08-30 17:17:05 +0900

    delete: UserController RequestMapping 삭제

--- src/main/java/com/example/mypetlife/controller/UserController.java
+++ src/main/java/com/example/mypetlife/controller/UserController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api")
 public class UserController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
