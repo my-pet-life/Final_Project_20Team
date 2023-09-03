3388f921959428a4fcadec0d220cee6712769a34
Sumin <csumin008255@gmail.com>
2023-09-04 00:25:39 +0900

    test: ErrorCode update2

--- src/main/java/com/example/mypetlife/exception/ErrorCode.java
+++ src/main/java/com/example/mypetlife/exception/ErrorCode.java
@@ -25,8 +25,10 @@ public enum ErrorCode {
      */
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
     NOT_FOUND_ARTICLE(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다"),
+    NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다"),
+    NOT_FOUND_SCHEDULE(HttpStatus.NOT_FOUND, "일정을 찾을 수 없습니다"),
+    NOT_FOUND_TAG(HttpStatus.NOT_FOUND, "태그를 찾을 수 없습니다"),
     NOT_FOUND_CHATROOM(HttpStatus.NOT_FOUND, "해당 채팅방을 찾을 수 없습니다."),
-
     /*
      * 409
      */
