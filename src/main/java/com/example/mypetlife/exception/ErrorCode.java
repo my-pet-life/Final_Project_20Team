7773c189edb34a5b9b72520d0c0f54175c4ab69f
sumin <csumin008255@gmail.com>
2023-09-03 23:06:14 +0900

    feat: 챗봇 관련 에러 메세지 추가

--- src/main/java/com/example/mypetlife/exception/ErrorCode.java
+++ src/main/java/com/example/mypetlife/exception/ErrorCode.java
@@ -18,17 +18,20 @@ public enum ErrorCode {
      * 401
      */
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다"),
+    UNAUTHORIZED_CHATROOM(HttpStatus.UNAUTHORIZED, "해당 채팅방에 접근 권한이 없습니다."),
 
     /*
      * 404
      */
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
     NOT_FOUND_ARTICLE(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다"),
+    NOT_FOUND_CHATROOM(HttpStatus.NOT_FOUND, "해당 채팅방을 찾을 수 없습니다."),
 
     /*
      * 409
      */
-    ALREADY_EXIST_USER(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다")
+    ALREADY_EXIST_USER(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다"),
+    ALREADY_EXIST_CHATROOM(HttpStatus.CONFLICT, "이미 채팅방을 생성하였습니다.")
 
     ;
 
