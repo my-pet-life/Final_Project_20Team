200c54e421936551a25185ae1b3dd946ecd0db13
shinjaewon99 <tlswodnjs99@naver.com>
2023-09-06 15:42:29 +0900

    feat : 서울 동물병원 리뷰 엔티티, 레포지토리 추가

---
+++ src/main/java/com/example/mypetlife/repository/review/SeoulReviewRepository.java
@@ -0,0 +1,17 @@
+package com.example.mypetlife.repository.review;
+
+import com.example.mypetlife.entity.hospital.SeoulHospital;
+import com.example.mypetlife.entity.review.SeoulReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface SeoulReviewRepository extends JpaRepository<SeoulReview, Long> {
+    @Query("SELECT review FROM SeoulReview review WHERE review.seoulHospitalId = :seoulHospitalId " +
+            "AND review.nextReviewId = :nextReviewId ")
+    Optional<SeoulReview> findBySeoulHospitalIdAndNextReviewId
+            (@Param("seoulHospitalId") final SeoulHospital seoulHospital, @Param("nextReviewId") final Long nextReviewId);
+}
+
