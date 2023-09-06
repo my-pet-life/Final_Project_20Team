6959e83bd324d0fd453c0b6277837aa567679f28
shinjaewon99 <tlswodnjs99@naver.com>
2023-09-06 15:43:00 +0900

    feat : 리뷰 관련 응답 DTO 추가

---
+++ src/main/java/com/example/mypetlife/dto/review/response/ReviewCommonResponse.java
@@ -0,0 +1,10 @@
+package com.example.mypetlife.dto.review.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ReviewCommonResponse {
+    private String message;
+}
