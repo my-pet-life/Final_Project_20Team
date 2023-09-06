7ecd4b9350186edcceafdae30f1f0b40aedf708d
shinjaewon99 <tlswodnjs99@naver.com>
2023-09-06 15:40:00 +0900

    remove : 사용하지 않은 Review 관련 클래스 삭제

--- src/main/java/com/example/mypetlife/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mypetlife.repository;
-
-import com.example.mypetlife.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
