292ea931234aa22d3d5fa980df7b4518bf928f94
shinjaewon99 <tlswodnjs99@naver.com>
2023-09-07 00:08:33 +0900

    refactor : security 동물병원 리뷰 url 수정

--- src/main/java/com/example/mypetlife/config/WebSecurityConfig.java
+++ src/main/java/com/example/mypetlife/config/WebSecurityConfig.java
@@ -1,34 +1,31 @@
 package com.example.mypetlife.config;
-
 import com.example.mypetlife.jwt.*;
 import com.example.mypetlife.oauth.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
-
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
-
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig {
-
     private final OAuth2UserService oAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final JwtTokenUtils jwtTokenUtils;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authHttp ->
                         authHttp.requestMatchers(
@@ -36,6 +33,8 @@ public class WebSecurityConfig {
                                         "/register",
                                         "/login/**",
                                         "/api/hospitals/**/",
+                                        "/api/seoul/reviews/**/",
+                                        "/api/gyeonggi/reviews/**/",
                                         "/","/v3/api-docs/**",
                                         "/swagger-ui/**",
                                         "/community/articles/**",
@@ -49,25 +48,47 @@ public class WebSecurityConfig {
                 .oauth2Login(oauth2Login -> oauth2Login
                         .loginPage("/login")
                         .successHandler(oAuth2SuccessHandler)
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(oAuth2UserService)
+                        .userInfoEndpoint(userInfo -> userInfo // oauth2 로그인 성공 후 가져올 때의 설정들
+                                .userService(oAuth2UserService) // 소셜로그인 성공 후 후속 조치를 진행할 userService
                         )
                 )
-
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler)
                 .and()
-
                 .sessionManagement(sessionManagment -> sessionManagment
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
+                // JwtFilter를 SecurityChainFilter에 등록하여 특정 URL에 접근하기 전에 로그인 유무를 확인한다.
                 .addFilterBefore(new JwtFilter(jwtTokenUtils), AuthorizationFilter.class)
+                // JwtExceptionFilter를 SecurityChainFilter에 등록하여 JwtFilter에서 발생한 예외를 처리한다.
+                // 즉 특정 URL에 로그인 없이 접근하면 예외가 발생하고, 해당 필터가 예외를 처리한다.
+//                .addFilterBefore(new JwtExceptionFilter(), JwtFilter.class)
         ;
-
         return http.build();
     }
-
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring()
+                    // 해당 경로는 security filter chain을 생략
+                    // 즉 permitAll로 설정하여 로그인 없이 접근 가능한 URL을 아래에 추가하여
+                    // 해당 URL 요청들은 JwtFilter, JwtExceptionFilter를 포함한 스프링 시큐리티의 필터 체인을 생략
+                    .requestMatchers(
+                            "/main",
+                            "/register",
+                            "/login/**",
+                            "/api/hospitals/**/",
+                            "/api/seoul/reviews/**/",
+                            "/api/gyeonggi/reviews/**/",
+                            "/","/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/community/search/**",
+                            "/hospitals/**",
+                            "/access_token")
+                    .requestMatchers(POST, "/sms/send/**")
+                    .requestMatchers(GET, "/community/articles/**");
+        };
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
