ced04f207625b83836992374d3f4de180107e065
olsohee <dlthgml0108@daum.net>
2023-09-10 21:35:48 +0900

    fix: deploy.sh 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/my-pet-life
+PROJECT_NAME=my-pet-life
+
 cd $REPOSITORY
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
-APP_NAME=my-pet-life
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+#APP_NAME=my-pet-life
+#JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+#JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+#CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 
 if [ -z $CURRENT_PID ]
 then
