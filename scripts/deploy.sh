da2d3fc4520b67375b47e21fd918ed57da7b344d
olsohee <dlthgml0108@daum.net>
2023-09-12 23:59:43 +0900

    fix: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,15 +14,13 @@ then
   echo "> 현재 구동 중인 애플리케이션 없음"
 else
   echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  kill -9 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
-
 echo "> Jar Name: $JAR_NAME"
-echo "> $JAR_NAME에 실행권한 추가"
-chmod +x $JAR_NAME
 
+echo "> 새 애플리케이션 배포"
+chmod +x $JAR_NAME
 nohup java -jar $REPOSITORY/$JAR_NAME 1>/home/ubuntu/log/output.log 2>/home/ubuntu/log/error.log &
