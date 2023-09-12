11e5ac36a97ff6c1ab6f9f2fbcf90229663ff699
olsohee <dlthgml0108@daum.net>
2023-09-12 09:39:20 +0900

    fix: 배포 관련 설정 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/my-pet-life
+REPOSITORY=/home/ubuntu
 PROJECT_NAME=my-pet-life
 
 cd $REPOSITORY
 cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 
+echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할것 없음."
+  echo "> 현재 구동 중인 애플리케이션 없음"
 else
   echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
