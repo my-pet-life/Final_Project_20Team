97231b3ba59f2e130f17d7b8182baa2b0f8431a4
Sumin <csumin008255@gmail.com>
2023-09-14 01:31:48 +0900

    feat: 메세지 조회 시 관리자일 경우 조회가능

--- src/main/java/com/example/mypetlife/controller/ChatController.java
+++ src/main/java/com/example/mypetlife/controller/ChatController.java
@@ -2,6 +2,8 @@ package com.example.mypetlife.controller;
 
 import com.example.mypetlife.dto.chat.ChatMessageListDto;
 import com.example.mypetlife.entity.user.User;
+import com.example.mypetlife.exception.CustomException;
+import com.example.mypetlife.exception.ErrorCode;
 import com.example.mypetlife.jwt.JwtTokenUtils;
 import com.example.mypetlife.repository.ChatRoomRepository;
 import com.example.mypetlife.service.ChatService;
@@ -9,6 +11,7 @@ import com.example.mypetlife.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,23 +27,32 @@ public class ChatController {
     private final ChatRoomRepository chatRoomRepository;
 
     // 채팅 메세지 조회
-    @GetMapping("/getChatting/{roomId}")
+    @GetMapping("/{roomId}")
     public List<ChatMessageListDto> startChat(
             HttpServletRequest request,
             @PathVariable("roomId") Long roomId) {
-        // 유저 권한 확인
-        chatService.userCheck(request, roomId);
-        return chatService.readMessage(roomId);
+        // 채팅을 생성한 유저이거나 관리자일 경우
+        if(chatService.userCheck(request, roomId) || chatService.adminCheck(request)) return chatService.readMessage(roomId);
+        throw new CustomException(ErrorCode.UNAUTHORIZED_CHATROOM);
     }
 
     // 채팅방 생성
-    @PostMapping("/createChat")
+    @PostMapping("/chatRoom")
     public String startChatbot(HttpServletRequest request) {
         String email = jwtTokenUtils.getEmailFromHeader(request);
         User user = userService.findByEmail(email);
 
         return chatService.createRoom(user);
     }
+
+    // 채팅 메세지 삭제
+//    @DeleteMapping("/{roomId}")
+//    public String deleteMessage(
+//            HttpServletRequest request,
+//            @PathVariable("roomId") Long roomId){
+//        // 유저 권한 확인
+//
+//    }
 
 
 }
--- src/main/java/com/example/mypetlife/service/ChatService.java
+++ src/main/java/com/example/mypetlife/service/ChatService.java
@@ -4,6 +4,7 @@ import com.example.mypetlife.dto.chat.ChatMessageDto;
 import com.example.mypetlife.dto.chat.ChatMessageListDto;
 import com.example.mypetlife.entity.ChatRoom;
 import com.example.mypetlife.entity.Message;
+import com.example.mypetlife.entity.user.Authority;
 import com.example.mypetlife.entity.user.User;
 import com.example.mypetlife.exception.CustomException;
 import com.example.mypetlife.exception.ErrorCode;
@@ -129,16 +130,24 @@ public class ChatService {
         return chatMessageList;
     }
 
-    // TODO 일반 유저일 경우 채팅방을 생성한 유저인지 확인, 관리자일 경우 통과
-    public void userCheck(HttpServletRequest request, Long roomId){
+    // TODO 채팅방을 생성한 유저이면 true 반환
+    public boolean userCheck(HttpServletRequest request, Long roomId){
         // 채팅방을 만든 사용자인지 확인
         String email = jwtTokenUtils.getEmailFromHeader(request);
         User user = userService.findByEmail(email);
 
         ChatRoom chatRoom = chatRoomRepository.findById(roomId)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_CHATROOM));
-        if(user != chatRoom.getChatRoomUser()) throw new CustomException(ErrorCode.UNAUTHORIZED_CHATROOM);
-        // TODO 관리자일 경우는 접근 가능
+        if(user != chatRoom.getChatRoomUser()) return false;
+        else return true;
+    }
 
+    // TODO 관리자면 true 반환
+    public boolean adminCheck(HttpServletRequest request){
+        String userEmail= jwtTokenUtils.getEmailFromHeader(request);
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+        if(user.getAuthority().equals(Authority.ROLE_ADMIN)) return true;
+        else return false;
     }
 }
