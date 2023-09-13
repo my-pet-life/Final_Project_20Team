c10197427ee0685b15a86b00a3786c07f77e5d29
jsl1113 <jsl1113@naver.com>
2023-09-13 16:52:09 +0900

    feat: ResponseBody 삭제

--- src/main/java/com/example/mypetlife/controller/view/ViewController.java
+++ src/main/java/com/example/mypetlife/controller/view/ViewController.java
@@ -3,9 +3,7 @@ package com.example.mypetlife.controller.view;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@ResponseBody
 @Controller
 @Slf4j
 public class ViewController {
